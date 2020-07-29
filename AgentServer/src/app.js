eceaed6543815f6672afd4eb2c722d06b1e781aa
garyparrot <weedbug666@gmail.com>
2020-07-29 23:32:33 +0800

    refactor(agent-server): clean up app.py code

--- AgentServer/src/app.js
+++ AgentServer/src/app.js
@@ -11,18 +11,9 @@ import authentication_router from './routers/authentication'
 /* TODO: Rewrite this shit with typescript */
 /* TODO: implement graceful shotdown for agent-server */
 
-async function create_app() {
-    const app = express();
-    const server = http.createServer(app);
-    const sessionMgr = await connectSessionStorage();
+function createHttpServer(express_app) { //{{{
+    const server = http.createServer(express_app);
 
-    app.use(sessionMgr);
-    app.use(bodyParser.json())
-
-    app.use(express_logger);
-    app.use('/', authentication_router);
-
-    /* Handle WebSocket upgrade process *///{{{
     server.on('upgrade', function(request, socket, head) {
 
         validateSession(request).then((userid) => { 
@@ -33,25 +24,28 @@ async function create_app() {
             socket.destroy();
         });
 
-    });//}}}
+    });
 
-    /* Server on close *///{{{
-    server.close = async function() {
-        function exception_handler(description) {
-            return (reason) => logger.error(description, { exception: reason })
-        }
-        logger.info(`Closing agent-server`)
-        await new Promise((resolve, reject) => {
-            const handler = (err) => { err ? reject(err) : resolve() }
-            Object.getPrototypeOf(server).close.bind(server,handler)();
-        });
+    server.on('close', function() {
+        logger.info(`Closing http server`)
 
         logger.info(`Closing connection to redis session storage`)
-        await disconnectSessionStorage()
-            .catch(exception_handler("failed to close session storage"))
+        disconnectSessionStorage().catch((reason) => logger.warn(`failed to close session storage`, {reason: reason}))
+    })
 
-    }//}}}
+    return server
+} //}}}
 
+async function create_app() {
+    const app = express();
+    const server = createHttpServer(app);
+    const sessionMgr = await connectSessionStorage();
+
+    app.use(sessionMgr);
+    app.use(bodyParser.json())
+
+    app.use(express_logger);
+    app.use('/', authentication_router);
     app.use(express_error_logger);
 
     server.listen(3000, function() {
