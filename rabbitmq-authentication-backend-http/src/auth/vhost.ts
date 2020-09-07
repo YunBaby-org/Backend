bbaac2926881c822d845b3a426333ed07bf45a5e
garyparrot <weedbug666@gmail.com>
2020-09-07 23:51:11 +0800

    build(backend): remove rabbitmq-authentication-backend-http

--- rabbitmq-authentication-backend-http/src/auth/vhost.ts
+++
@@ -1,8 +0,0 @@
-export interface VhostRequestParam {
-  username: string;
-  vhost: string;
-  ip: string;
-}
-export function authVhost({username, vhost, ip}: VhostRequestParam): boolean {
-  return vhost === 'user';
-}
--- rabbitmq-authentication-backend-http/src/utility/response.ts
+++
@@ -1,7 +0,0 @@
-export function errorResponse(reason: string) {
-  return {status: 'failed', reason: reason};
-}
-
-export function successResponse(payload: object) {
-  return {status: 'success', ...payload};
-}
