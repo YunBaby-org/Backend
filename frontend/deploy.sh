1e06a53561506f19ae6d93c970715cce24e75567
garyparrot <weedbug666@gmail.com>
2020-09-07 23:52:02 +0800

    build(backend): remove frontend

--- frontend/deploy.sh
+++
@@ -1,9 +0,0 @@
-
-npm run build
-cd dist
-git init
-git add -A
-git commit -m 'deploy'
-# 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
-git push -f https://github.com/Eric07110904/final.git master:gh-pages
-
--- frontend/src/store/getters.js
+++
@@ -1,8 +0,0 @@
-const getters = {
-    sidebar:(state)=>state.app.sidebar,
-    userinfo:(state)=>state.user.userinfo,
-    trackerList:(state)=>state.trackers.tracker_list,
-    trackersInfo:(state)=>state.trackers.trackers
-}
-
-export default getters
